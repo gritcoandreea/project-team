ca0d1e6511a1e0092549bdd2d2930fc3b241363a
Gritco Andreea <gritco.andreea@gmail.com>
2020-03-15 21:53:42 +0200

    First commit.

---
+++ src/main/java/ro/project/team/AbacTeamApplication.java
@@ -0,0 +1,13 @@
+package ro.project.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/project/team/AbacTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.project.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
